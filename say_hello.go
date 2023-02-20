cc4d4f35b324a240ef7227ddddc63fe692ee5694
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2023-02-20 08:56:06 +0700

    update say hello

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,8 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello"
+func SayHello() string { //nama func Uppercase agar diset sebagai public
+	return "Hello World"
 }
+
+//go mod init nama-module u/ membuat module baru
+//go get nama-module u/menambahkan dependency dimodule kita
